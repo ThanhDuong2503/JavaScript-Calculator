3e6fa6420a793125cf746938d5b57a743ad68b21
Thanh <Thanh.Duong2503@gmx.de>
2020-06-08 14:35:16 +0200

    addFunctions

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 let firstNumber = Number(prompt("please enter your first number", "5"));
 let secondNumber = Number(prompt("please enter your second number", "10"));
 let operator = prompt("please select an operation", ["+ ,- ,* , /"]);
-let result;
 
-if(operator == "+") {
-    result = firstNumber + secondNumber;
-} else if (operator == "-") {
-    result = firstNumber - secondNumber;
-} else if (operator == "*") {
-    result = firstNumber * secondNumber;
-} else if (operator == "/") {
-    result = firstNumber / secondNumber;
-} else {
-    result="invalid";
+// let result;
+
+// if(operator == "+") {
+//     result = firstNumber + secondNumber;
+// } else if (operator == "-") {
+//     result = firstNumber - secondNumber;
+// } else if (operator == "*") {
+//     result = firstNumber * secondNumber;
+// } else if (operator == "/") {
+//     result = firstNumber / secondNumber;
+// } else {
+//     result="invalid";
+// }
+//
+// alert("the result is " + result);
+
+function add(firstNumber, secondNumber){
+    return firstNumber + secondNumber;
+}
+function sub(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+}
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+}
+function div(firstNumber, secondNumber){
+    return firstNumber / secondNumber;
 }
 
-alert("the result is " + result);
+if(operator == "+") {
+    let result = add(firstNumber , secondNumber);
+    alert("the result is " + result);
+} else if (operator == "-") {
+    let result = sub(firstNumber , secondNumber);
+    alert("the result is " + result);
+} else if (operator == "*") {
+    let result = multiply(firstNumber , secondNumber);
+    alert("the result is " + result);
+} else if (operator == "/") {
+    let result = div(firstNumber , secondNumber);
+    alert("the result is " + result);
+} else {
+    alert("invalid input");
+}
+
